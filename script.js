122f8873e2afc73b9f8a5fc7778e7bab051d81a1
Megan Costello <costelmx@bc.edu>
2021-09-22 13:43:41 -0400

    two types of charts with d3

---
+++ script.js
@@ -0,0 +1,134 @@
+async function scatter(){
+    let cityData;
+
+    await d3.csv('cities.csv', d3.autoType).then(data=>{
+        cityData = data;
+        console.log('cities', data);
+        
+    })
+    console.log('city data ', cityData);
+    // for (let x=0; x<cityData.length; x++) {
+    //     console.log(cityData[x].eu);
+    // }
+    let europeanCountries = cityData.filter(country => country.eu == true);
+    console.log('euro cities ', europeanCountries);
+    d3.select('.city-count').text('Number of cities: ' + europeanCountries.length);
+
+    const width = 700;
+    const height = 550;
+    const svg = d3.select('.population-plot')
+        .append('svg')
+        .attr('width', width)
+        .attr('height', height)
+
+    svg.selectAll("circle")
+        .data(europeanCountries)
+        .enter()
+        .append("circle")
+        .attr('cx', function (d, i){
+            // d: datum
+            // i: index
+            return d.x;
+        })
+        .attr('cy', function (d, i){
+            return d.y;
+        })
+        .attr("r", function (d, i){
+            return (d.population < 1000000) ? 4 : 8;
+        })
+        .attr("fill", "orange");
+
+    let bigCities = europeanCountries.filter(country => country.population > 1000000);
+    
+    svg.selectAll("text")
+        .data(bigCities)
+        .enter()
+        .append("text")
+        .text(function (d, i){
+        return d.country;
+        })
+        .attr("dx", function (d, i){
+            return d.x;
+        })
+        .attr("dy", function (d, i){
+            return d.y-12;
+        })
+        .attr("font-size", 11)
+        .attr("text-anchor", "middle");
+};
+        
+scatter();
+
+async function bar(){
+    let buildingData;
+
+    await d3.csv('buildings.csv', d3.autoType).then(data=>{
+        buildingData = data.sort(function(a, b){return b.height_m - a.height_m});
+        console.log('buildings ', data);
+        console.log('buildings sort', buildingData);
+        
+    })
+
+    const width = 500;
+    const height = 500;
+    const svg = d3.select('.building-height-chart')
+        .append('svg')
+        .attr('width', width)
+        .attr('height', height);
+    
+    svg.selectAll("rect")
+        .data(buildingData)
+        .enter()
+        .append("rect")
+        .attr('height', 40)
+        .attr('width', function (d, i){
+            return d.height_px;
+        })
+        .attr('x', 250)
+        .attr('y', function (d, i){
+            return i*(50);
+        })
+        .attr("fill", "orange")
+        .on("click", function(d, i) {
+            // Do something after clicking a bar
+            
+                d3.select('.image').attr("src", ("/img/img/" + (buildingData.indexOf(i)+1)+".jpg"));
+                d3.select('.building-name').text(i.building);
+                d3.select('.height').text(i.height_ft);
+                d3.select('.city').text(i.city);
+            });
+
+    let texts = svg.selectAll("text")
+                .data(buildingData)
+                .enter();
+
+    
+    texts.append("text")
+        .text(function (d, i){
+        return d.building;
+        })
+        .attr("dx", 10)
+        .attr("dy", function (d, i){
+            return i*(50)+25;
+        })
+        .attr("font-size", 15);
+
+    texts.append("text")
+        .text(function (d, i){
+        return d.height_ft + " ft";
+        })
+        .attr("dx",  function (d, i){
+            return 220+d.height_px;
+        })
+        .attr("dy", function (d, i){
+            return i*(50)+25;
+        })
+        .attr("text-anchor", "end")
+        .attr("font-size", 15)
+        .attr("fill", "white");
+    
+   
+        
+}
+
+bar();
